274cac473d80d0c6a73472d950c2373aa5b93e80
Lloyd Holman <lloyd.holman@datumgenerics.net>
2019-05-11 22:35:29 +0100

    Add Travis config to build response and cron Dockerfiles and publish to ECS

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+$REPO=$1
+docker build -t $REPO:$TAG -f Dockerfile .
+docker --version  
+pip install --user awscli
+export PATH=$PATH:/$HOME/.local/bin
+eval $(aws ecr get-login --registry-ids $AWS_ACCOUNT_NUMBER --region eu-west-2 --no-include-email)
+docker tag $REPO:$TAG $AWS_ACCOUNT_NUMBER.dkr.ecr.eu-west-2.amazonaws.com/$REPO:$TAG
+docker push $AWS_ACCOUNT_NUMBER.dkr.ecr.eu-west-2.amazonaws.com/$REPO:$TAG
