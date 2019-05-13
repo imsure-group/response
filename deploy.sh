aba960ff8cabbce0c3676311422f9ee85a0bcd68
Lloyd Holman <lloyd.holman@datumgenerics.net>
2019-05-13 08:16:36 +0100

    Only build containers once, install AWS CLI

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 REPO=$1
+DOCKERFILE=$2
 echo "Running deployment for container: $REPO"
-docker build -t $REPO:$TAG -f Dockerfile .
+docker build -t $REPO:$TAG -f $DOCKERFILE .
 docker --version  
-pip install --user awscli
+pip install awscli
 export PATH=$PATH:/$HOME/.local/bin
 eval $(aws ecr get-login --registry-ids $AWS_ACCOUNT_NUMBER --region eu-west-2 --no-include-email)
 docker tag $REPO:$TAG $AWS_ACCOUNT_NUMBER.dkr.ecr.eu-west-2.amazonaws.com/$REPO:$TAG
